b15467edfca0022bc2907aee76bf89bd4ab2e656
mrk25 <mrk25@localhost.localdomain>
2014-11-07 09:25:57 +0100

    click al posto di tap su simbolo azione in modalita presentazione

--- js/topodog-nogrid.js
+++ js/topodog-nogrid.js
@@ -875,7 +875,7 @@ topoDog = { // Oggetto base con parametri fondamentali
 					var closeNote = false;
 					$('.action').on({
 						
-						'tap':function(){
+						'click':function(){
 							//console.log($(this).attr('data-id'));
 							if(closeNote == true){
 								$('#noteShow, #noteForm').remove();
@@ -921,6 +921,7 @@ topoDog = { // Oggetto base con parametri fondamentali
 						
 						}
 					});
+					
 					
 					if(preserveTimeline === true){
 						scroll_timeline.refresh();
